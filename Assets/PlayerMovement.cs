fa46e372375f177e55376affc40a56cb81e4bd2a
DeepikaVasam123 <deepika.vasam@xcubelabs.com>
2021-05-26 15:02:24 +0530

    Player moment done

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    Vector3 dir;
+    public float playerSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(dir == Vector3.forward)
+            {
+                dir = Vector3.left;
+            }
+            else
+            {
+                dir = Vector3.forward;
+            }
+        }
+        transform.Translate(dir * playerSpeed * Time.deltaTime);
     }
 }
